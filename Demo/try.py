f6869d89cdad1c289e94d36d622dc691af5547cb
Preksha Sharma <46593513+prektrons@users.noreply.github.com>
2023-04-23 10:54:25 +0530

    Spark-Created RDD with Parallelizing

--- Demo/try.py
+++ Demo/try.py
@@ -4,3 +4,11 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.master('local').appName('TEST').getOrCreate()
 
 print("Spark object Created")
+
+# SparkContext is the entry point to any spark functionality. pyspark.SparkContext is an entry point to the PySpark
+# functionality that is used to communicate with the cluster and to create an RDD, accumulator, and broadcast variables
+
+# Parallelizing the spark application distributes the data across the multiple nodes and is used to process the data
+# in the Spark ecosystem.
+rdd = spark.sparkContext.parallelize([1, 2, 3])
+print(rdd.first())
