ca6d59e72ae552635f508d6fdae02e51a669e507
Preksha Sharma <46593513+prektrons@users.noreply.github.com>
2023-04-23 10:37:31 +0530

    Spark build app

--- Demo/try.py
+++ Demo/try.py
@@ -1,2 +1,6 @@
-import sys
-print(sys.argv[0])
+from pyspark.sql import SparkSession
+
+# creating spark object
+spark = SparkSession.builder.master('local').appName('TEST').getOrCreate()
+
+print("Spark object Created")
